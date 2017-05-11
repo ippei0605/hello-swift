5099f0666db0458089b53728c7a5c3b8119db2ea
Ippei SUZUKI <i.suzuki@jiec.co.jp>
2017-05-11 11:17:35 +0900

    Initial Commit

---
+++ hello-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello-swift
+//
+//  Created by Ippei SUZUKI on 2017/05/11.
+//  Copyright © 2017年 Ippei SUZUKI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
